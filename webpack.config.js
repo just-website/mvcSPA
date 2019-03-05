eb4eaa769a64028b6ea7ea3f4aee601cf1bd1d62
Gleb Shchukin <just-website@yandex.ru>
2019-03-06 00:32:25 +0300

    rebuild project

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ const conf = {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
+                    use: ['css-loader?url=false', 'sass-loader']
                 })
             }
         ]
